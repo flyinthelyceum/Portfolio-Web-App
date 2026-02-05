889dec166ad02b7adce3f111b0af4bbc8231279b
flyinthelyceum <jreasy@gmail.com>
2026-02-04 20:08:58 -0700

    Fix Log Out: Redirect to homepage immediately after signing out

--- editor.js
+++ editor.js
@@ -119,6 +119,7 @@ function setupEventListeners() {
   // Logout
   document.getElementById('logout-btn').addEventListener('click', async () => {
     await signOut(auth);
+    window.location.href = 'index.html';
   });
 
   // Share portfolio
